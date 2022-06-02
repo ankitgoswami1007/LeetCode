bc4b5502944987ac7795baa8faf322f47b2bfd6a
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2022-06-02 17:21:20 +0530

    Time: 233 ms (14.30%), Space: 93.3 MB (89.80%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& arr) {
+    int maxProfit(vector<int>& prices) {
         
-        int ans = 0;
-        int minprice = INT_MAX;
-        
-        for(int i = 0 ; i < arr.size() ; i++){
-            minprice = min(minprice, arr[i]);
-            ans = max(ans , arr[i] - minprice);   
+        int buyStock = INT_MAX;
+        int sellStock = INT_MIN;
+        int res = 0;
+        for(auto& p : prices){
+            buyStock =min(buyStock , p);
+            sellStock = max(sellStock , p-buyStock);
+            
         }
-        return ans ;
+        if(sellStock > 0) res = sellStock ;
+        return  res ;
     }
 };
 
 // class Solution {
 // public:
-//     int maxProfit(vector<int>& prices) {
+//     int maxProfit(vector<int>& arr) {
         
-//         int buyStock = INT_MAX;
-//         int sellStock = INT_MIN;
-//         int res = 0;
-//         for(auto& p : prices){
-//             buyStock =min(buyStock , p);
-//             sellStock = max(sellStock , p-buyStock);
-            
+//         int ans = 0;
+//         int minprice = INT_MAX;
+        
+//         for(int i = 0 ; i < arr.size() ; i++){
+//             minprice = min(minprice, arr[i]);
+//             ans = max(ans , arr[i] - minprice);   
 //         }
-//         if(sellStock > 0) res = sellStock ;
-//         return  res ;
+//         return ans ;
 //     }
 // };
+
