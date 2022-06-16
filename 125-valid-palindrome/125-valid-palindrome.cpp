3c70c67c5b7df0cfe962869c6bae55b84da362b9
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2022-06-16 16:27:44 +0530

    Time: 14 ms (15.59%), Space: 7.3 MB (75.06%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(string s) {
        
-        transform(s.begin(), s.end(), s.begin(), :: tolower);
+       /* transform(s.begin(), s.end(), s.begin(), :: tolower);
         string a;
         for( auto& ch : s){
             
@@ -17,6 +17,18 @@ public:
             if(a[i] != a[j]) return false;
             
             i++; j--;  
+        }
+        return true ; */
+        
+        int st = 0 , end = s.size()-1;
+        
+        while(st < end) {
+            
+            if( ! isalnum(s[st])) st++;
+            else if( ! isalnum(s[end])) end--;
+            else{
+               if(tolower(s[st++]) != tolower(s[end--])) return false ;
+            } 
         }
         return true ;
     }
