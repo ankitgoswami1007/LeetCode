dc5c8e29fcb5e209bd0dc3e898d14be921f0f53f
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2022-06-26 22:13:32 +0530

    Time: 579 ms (73.20%), Space: 129.1 MB (56.69%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
         long long ans = 0;
@@ -32,5 +32,24 @@ public:
         }
         
         return ans ;
+    }
+}; */
+
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        long long ans = 0, x = 1;
+        vector<int> degree(n,0);
+        for(auto road : roads){
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+        sort(degree.begin(),degree.end());
+        for(auto i : degree){ 
+            //cout<<i<<" "<<x<<"\n";
+            ans += i * (x++);
+            //cout<<ans<<'\n';
+        }
+        return ans;
     }
 };
