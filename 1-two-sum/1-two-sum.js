3e779e36300c246a77b4f96cd9c92fbc3922e903
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2022-07-06 13:59:31 +0530

    Time: 105 ms (64.37%), Space: 42.8 MB (45.09%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,13 +5,26 @@
  */
 var twoSum = function(nums, target) {
     
-    
-    
-    for(let i = 0 ; i < nums.length-1 ; i++ ) {
+    /*for(let i = 0 ; i < nums.length-1 ; i++ ) {
         for(let j = i+1 ; j < nums.length ; j++){
             if(nums[i] + nums[j] == target) return [i , j];
         }
     }
-    return -1;
+    return -1; */
+    
+    let map = new Map();
+    let res = [];
+    for(let i = 0 ; i < nums.length ; i++){
+        
+        if(map.has(target-nums[i])){
+            res.push(map.get(target-nums[i]));
+            res.push(i);
+            return res;
+        }
+        else{
+            map.set(nums[i], i);
+        }
+    }
+    
     
 };
