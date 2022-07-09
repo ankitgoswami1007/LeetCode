c565fce52d80c59e85a893b214ec60b6149e031e
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2022-07-10 02:55:19 +0530

    Time: 519 ms (81.79%), Space: 178.6 MB (16.43%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,73 +1,72 @@
-class node{
-    public:
-    int key , value;
-    node* prev;
-    node* next;
-    
-    node(int k , int v):key(k),value(v),prev(NULL),next(NULL){}
-};
-
 class LRUCache {
-    unordered_map <int, node*> cache ;
-    int cap;
-    node* left ;
-    node* right ;
+    class Node{
+    public:
+        int key ;
+        int value ;
+        Node* prev;
+        Node* next;
+        public:
+        Node(int k, int v){
+            key = k;
+            value = v;
+            prev = NULL;
+            next = NULL;
+        }
+    };
+    int capacity ;
+    Node* head, *last ;
+    unordered_map<int, Node*> cache;
+    
 public:
-    
     LRUCache(int capacity) {
+        this->capacity = capacity;
+        head = new Node(0,0);
+        last = new Node(0,0);
         
-        cap = capacity;
-        left = new node(0,0);
-        right =new node(0,0);
+        head->next = last;
+        last->prev = head;
         
-        left->next = right;
-        right->prev = left;
-    }
-    void  removeAtLeft(node* n){
-        node* nxt = n->next ;
-        node* pre = n->prev;
-        
-        pre->next = nxt;
-        nxt->prev = pre;
     }
     
-    void insertAtRight(node* n){
-        node* pre = right->prev;
-        
-        n->next = right ;
-        n->prev = pre;
-        right->prev = n;
-        pre->next = n;
-        
-        
-    }
     int get(int key) {
         
         if(cache.find(key) != cache.end()){
-            removeAtLeft(cache[key]);
-            insertAtRight(cache[key]);
-            node* v = cache[key];
-            return v->value ;
+            Node* n = cache[key];
+            
+            remove(n);
+            insert(n);
+            return n->value;
         }
         return -1;
+        
     }
     
     void put(int key, int value) {
         
         if(cache.find(key) != cache.end()){
-            removeAtLeft(cache[key]);
+            remove(cache[key]);
         }
-        node* newnode = new node(key , value);
-        cache[key] = newnode;
-        insertAtRight(cache[key]);
-        
-        if(cache.size() > cap){
-            node* lru = left->next;
-            removeAtLeft(lru);
-            cache.erase(lru->key);
+        if(cache.size() == capacity){
+            remove(last->prev);
         }
+        insert(new Node(key , value));
         
+    }
+    
+    void insert(Node* node){
+        int key = node->key;
+        cache[key] = node;
+        node->next = head->next;
+        head->next->prev = node;
+        node->prev = head;
+        head->next = node;
         
+    }
+    
+    void remove(Node* node){
+        cache.erase(node->key);
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
     }
 };
 
