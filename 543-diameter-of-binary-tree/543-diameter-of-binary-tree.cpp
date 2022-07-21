a08c44a92a5276c6ae44d1c8ee7d919589629e3f
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2022-07-21 15:48:24 +0530

    Time: 24 ms (26.08%), Space: 20.3 MB (65.08%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -14,7 +14,7 @@ class Solution {
         if(root ==  NULL) return 0;
         int lh = diameter(root->left , ans);
         int rh = diameter( root -> right , ans);
-        
+
         ans = max(ans , lh+rh);
         return max(lh , rh) + 1;
     }
