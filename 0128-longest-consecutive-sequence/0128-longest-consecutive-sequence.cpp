15b2f580045846f01820a68cea61225c4ab55dc0
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2023-06-17 15:07:23 +0530

    Time: 791 ms (16.07%), Space: 67 MB (39.96%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-
+        // Map to store whether a number is present in the input array
         unordered_map<int, bool> umap;
 
-        for(auto & num : nums){
+        // Mark all numbers as present in the map
+        for(auto& num : nums) {
             umap[num] = true;
         }
 
-        for(auto & num : nums){
-            if(umap.find(num -1) != umap.end()) umap[num] = false;
+        // Iterate through the numbers
+        // If a number has a predecessor (num-1), mark it as false in the map
+        for(auto& num : nums) {
+            if(umap.find(num - 1) != umap.end()) {
+                umap[num] = false;
+            }
         }
 
         int max_start_point = 0;
         int max_len = 0;
-        for(auto & num : nums){
+
+        // Iterate through the numbers
+        // For each number that is the start of a sequence, calculate its length
+        // Update the maximum length and starting point if necessary
+        for(auto& num : nums) {
             if(umap[num] == true) {
                 int temp_len = 1;
                 int temp_start_pnt = num;
 
-                while(umap.find(temp_start_pnt + temp_len) != umap.end()){
+                // Calculate the length of the consecutive sequence
+                while(umap.find(temp_start_pnt + temp_len) != umap.end()) {
                     temp_len++;
                 }
+
+                // Update the maximum length and starting point if the current sequence is longer
                 if(temp_len > max_len) {
                     max_len = temp_len;
                     max_start_point = temp_start_pnt;
                 }
             }
         }
-        for(int i = 0 ; i < max_len ; i++){
-            cout<<(max_start_point+i)<<" ";
+
+        // Print the consecutive sequence with maximum length
+        for(int i = 0 ; i < max_len ; i++) {
+            cout << (max_start_point + i) << " ";
         }
+
         return max_len;
     }
 };
