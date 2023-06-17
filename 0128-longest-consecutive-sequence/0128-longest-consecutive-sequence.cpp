e9886df16bbe3412ebc450afd44be96014ba86b6
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2023-06-17 14:58:41 +0530

    Time: 646 ms (25.6%), Space: 67.1 MB (29.32%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        unordered_map<int, bool> umap;
+
+        for(auto & num : nums){
+            umap[num] = true;
+        }
+
+        for(auto & num : nums){
+            if(umap.find(num -1) != umap.end()) umap[num] = false;
+        }
+
+        int max_start_point = 0;
+        int max_len = 0;
+        for(auto & num : nums){
+            if(umap[num] == true) {
+                int temp_len = 1;
+                int temp_start_pnt = num;
+
+                while(umap.find(temp_start_pnt + temp_len) != umap.end()){
+                    temp_len++;
+                }
+                if(temp_len > max_len) {
+                    max_len = temp_len;
+                    max_start_point = temp_start_pnt;
+                }
+            }
+        }
+        return max_len;
+    }
+};
