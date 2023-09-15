d1e28d211cf7e0220be7e1d18729ff94e2839479
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2023-09-15 11:44:56 +0530

    Time: 1153 ms (16.35%), Space: 14.5 MB (30.75%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,41 +1,28 @@
-struct pair_hash
-{
-    template <class T1, class T2>
-    std::size_t operator () (std::pair<T1, T2> const &pair) const
-    {
-        std::size_t h1 = std::hash<T1>()(pair.first);
-        std::size_t h2 = std::hash<T2>()(pair.second);
- 
-        return h1 ^ h2;
-    }
-};
-
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
+        vector<int> left(26);
+        vector<int> right(26);
+        unordered_set<string> st;
 
-        unordered_set<int> left;
-        std::unordered_set<std::pair<int, int>, pair_hash> res;
-        unordered_map<int, int> right;
-
-        for(int i = 0 ; i < s.size(); i++){
-            right[s[i]]++;
+        for(auto ch : s){
+            right[ch-'a']++;
         }
 
-        for(int i = 0 ; i < s.size(); i++){
-            right[s[i]] -= 1;
-            if(right[s[i]] == 0){
-                right.erase(s[i]);
-            }
+        for(int i = 0 ; i < s.size() ; i++){
+            right[s[i]-'a']--;
             for(int j = 0 ; j < 26 ; j++){
-                char ch = j+'a';
-                if(left.find(ch) != left.end() && right.find(ch) != right.end()){
-                    res.insert({s[i], ch});
+                if(left[j] > 0 && right[j]> 0){
+                    char ch = j + 'a';
+                    string res = "";
+                    res += ch; res += s[i]; res += ch;
+                    
+                    if(st.count(res)) continue;
+                    st.insert(res);
                 }
             }
-            left.insert(s[i]);
+            left[s[i]-'a']++;
         }
-        return res.size();
-        
+        return st.size();
     }
 };
