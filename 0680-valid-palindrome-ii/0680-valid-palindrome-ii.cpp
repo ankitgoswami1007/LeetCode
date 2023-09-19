a6f2d077f3580a75836b82d1602f1bc1c4a48ada
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2023-09-19 14:59:32 +0530

    Time: 66 ms (5.4%), Space: 19.9 MB (73.95%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int start=0;
+        int n = s.size();
+        int end = n-1;
+        int c1 = 0 , c2 = 0;
+
+        while(start <= end){
+            if(s[start] == s[end]){
+                start++; end--;
+            }else{
+                start++;
+                c1++;
+            }
+        }
+        start = 0;
+        end = n-1;
+        while(start <= end){
+            if(s[start] == s[end]){
+                start++; end--;
+            }else{
+                end--;
+                c2++;
+            }
+        }
+        if(c1 == 0 || c2 == 0) return true;
+        if(c1 == 1 || c2 == 1) return true;
+        return false;
+    }
+};
