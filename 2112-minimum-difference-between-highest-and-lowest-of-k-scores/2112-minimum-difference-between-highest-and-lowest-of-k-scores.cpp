d7429e4efff0a633d2d671acdee135e2f479821d
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2023-09-20 10:43:57 +0530

    Time: 11 ms (71.78%), Space: 13.9 MB (10.7%) - LeetHub

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/2112-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/2112-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -3,11 +3,11 @@ public:
     int minimumDifference(vector<int>& nums, int k) {
         int mn = INT_MAX;
         int  n = nums.size();
-        if(n == 1 ) return 0;
+        int j = k-1, i = 0;
         sort(nums.begin(), nums.end());
 
-        for(int i = 0 ; i+k-1 < n ; i++){
-            mn = min(mn, nums[i+k-1]-nums[i]);
+        while(j < n) {
+            mn = min(mn, nums[j++]- nums[i++]);
         }
         return mn;
     }
