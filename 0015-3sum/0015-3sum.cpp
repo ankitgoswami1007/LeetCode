0520c251bcd18d448436632074a5c4fe2fe8ba43
ankitgoswami1007 <102970787+ankitgoswami1007@users.noreply.github.com>
2023-09-25 13:19:42 +0530

    Time: 103 ms (80.77%), Space: 24.3 MB (68.98%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,51 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;  // Result vector to store the triplets
-        if(nums.size() < 3) return res;  // If there are less than 3 elements, return empty result
-        
-        sort(nums.begin(), nums.end());  // Sort the input array in ascending order
-        
-        for(int i = 0; i < nums.size()-2; i++) {
-            // Skip duplicate values for the first element of the triplet
-            if(i == 0 || (i > 0 && nums[i] != nums[i-1])) {
-                int low = i + 1, high = nums.size() - 1, sum = 0 - nums[i];  // Set pointers for the remaining two elements
-                
-                while (low < high) {
-                    if(nums[low] + nums[high] == sum) {
-                        vector<int> a;
-                        a.push_back(nums[i]);
-                        a.push_back(nums[low]);
-                        a.push_back(nums[high]);
-                        res.push_back(a);  // Found a valid triplet, add it to the result
-                        
-                        // Skip duplicate values for the remaining two elements
-                        while(low < high && nums[low] == nums[low+1]) low++;
-                        while(low < high && nums[high] == nums[high-1]) high--;
-                        
-                        low++;
-                        high--;
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
                     }
-                    else if(nums[low] + nums[high] < sum) {
-                        low++;  // Move the low pointer to increase the sum
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
                     }
-                    else {
-                        high--;  // Move the high pointer to decrease the sum
-                    }
+                    j++;
+                    k--;
                 }
-            } 
+            }
         }
-        
-        return res;  // Return the result vector
+        return ans;
     }
 };
-
-
-/* 
-*The time complexity of this code is O(n^2), where n is the number of elements in the input array nums.
-
-The initial sorting operation takes O(n log n) time complexity.
-The outer loop iterates over each element of nums, which takes O(n) time.
-Within the outer loop, the inner while loop performs a two-pointer approach, which takes O(n) time in the worst case scenario.
-Overall, the time complexity is dominated by the sorting operation, resulting in O(n log n).
-*/
